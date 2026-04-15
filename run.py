6f209a4d8fa587ac4bc76ba9ec4df78407b00340
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-15 19:05:43 +0900

    feat: dunders

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-print("hello worlds")
+# Dunders __builtins__, __init__
+message = "in python everything is object "
+print(message)
+
+result = type(message)
+print("result:", result)
+
+'''
+    In python there are builtin tools:
+        (1) Types > int float str list dict 
+        (2) Functions > print() len() input() type() str() int()
+        (3) Constants > True False None
+'''
+
+print(__builtins__)

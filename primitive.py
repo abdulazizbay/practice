2c5faf474f8a375c8ce3743dc7e83a5e4ec0c7ea
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-15 23:48:02 +0900

    feat: learn string in python

---
+++ primitive.py
@@ -0,0 +1,26 @@
+# print("==========")
+
+# # in Js , variable is a name of storage location
+# # in python, variable is a named reference
+
+# count = 100
+# count_type = type(count)
+# print(f"the count: {count} and type: {count_type}")
+
+# result1 = count.bit_count()
+# result2 = count.numerator
+# print(result1, result2)
+
+# print("====  string ======")
+
+# course = "AI Python Fullstack"
+# result = type(course)
+# print(f"the type of course: {course}")
+# result = course.title()
+# print(f"result1 : {result}")
+
+# result = course.upper()
+# print(f"result2 : {result}")
+
+# result = course.replace("Fullstack", "Masterclass")
+# print(f"result3 : {result}")

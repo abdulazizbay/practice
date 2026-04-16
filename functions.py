fa88cbada7706c7e0322b5b52683250678266cad
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-16 16:58:41 +0900

    feat: func scope

--- functions.py
+++ functions.py
@@ -1,30 +1,54 @@
-''' Functions 
-    (1) Define and Call
-    (2) Parametr va Argument
-    (3) Keyword and default Arguments
-    (4) Scope
-'''
+# ''' Functions
+#     (1) Define and Call
+#     (2) Parametr va Argument
+#     (3) Keyword and default Arguments
+#     (4) Scope
+# '''
 
-print("===== Define(parametr) and Call(argument) =====")
-# Built-in funtion => print() type()
-# Function - reusable block of code
-# instead of block {} in Java, Python uses indentation
+# print("===== Define(parametr) and Call(argument) =====")
+# # Built-in funtion => print() type()
+# # Function - reusable block of code
+# # instead of block {} in Java, Python uses indentation
 
-# define - parametr
-
-
-def greet(a):
-    print(f"how do u do, {a}")
+# # define - parametr
 
 
-def greeting(b):
-    print("greeting is executed")
-    return f"Hi, {b}"
+# def greet(a):
+#     print(f"how do u do, {a}")
 
 
-# Call - argument
-result = greet("abu")
-print("result: ", result)
+# def greeting(b):
+#     print("greeting is executed")
+#     return f"Hi, {b}"
 
-result2 = greeting("abu")
-print("result2: ", result2)
+
+# # Call - argument
+# result = greet("abu")
+# print("result: ", result)
+
+# result2 = greeting("abu")
+# print("result2: ", result2)
+
+print("=== Keyword & default arguments ===")
+
+# Define
+
+
+def give_greet(name, age=43):
+    print("give_greet is executed")
+    return f"hi {name}, u r {age}-years old"
+
+
+result3 = give_greet(name="abu")
+print(result3)
+
+print("=== Scope ===")
+b = 100
+
+
+def calculate(a):
+    c = a + b
+    print(c)
+
+
+calculate(2)

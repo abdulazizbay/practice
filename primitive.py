8424e5202306554db40eed1f1af2dda96537280e
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-16 16:17:15 +0900

    feat: learn primitives

--- primitive.py
+++ primitive.py
@@ -1,26 +1,41 @@
-# print("==========")
+print("==========")
 
-# # in Js , variable is a name of storage location
-# # in python, variable is a named reference
+# in Js , variable is a name of storage location
+# in python, variable is a named reference
 
-# count = 100
-# count_type = type(count)
-# print(f"the count: {count} and type: {count_type}")
+count = 100
+count_type = type(count)
+print(f"the count: {count} and type: {count_type}")
 
-# result1 = count.bit_count()
-# result2 = count.numerator
-# print(result1, result2)
+result1 = count.bit_count()
+result2 = count.numerator
+print(result1, result2)
 
-# print("====  string ======")
+print("====  string ======")
+# Methods: upper(), lower(), title(), find(), replace()
 
-# course = "AI Python Fullstack"
-# result = type(course)
-# print(f"the type of course: {course}")
-# result = course.title()
-# print(f"result1 : {result}")
+course = "AI Python Fullstack"
+result = type(course)
+print(f"the type of course: {course}")
+result = course.title()
+print(f"result1 : {result}")
 
-# result = course.upper()
-# print(f"result2 : {result}")
+result = course.upper()
+print(f"result2 : {result}")
 
-# result = course.replace("Fullstack", "Masterclass")
-# print(f"result3 : {result}")
+result = course.replace("Fullstack", "Masterclass")
+print(f"result3 : {result}")
+
+print("==== boolean =====")
+
+y = input("give ur value for y: ")
+result = y.isnumeric()
+print(f"result is numeric: {result}")
+
+# Truthy =>  all others
+# Falsy => false, 0 , "", none
+test_falsy = "" or False or None or 0
+print("the test falsy:", bool(test_falsy))
+
+test_truthy = "dsds"
+print("the test truthy:", bool(test_truthy))

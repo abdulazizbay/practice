bc4daedf75ec403862e51fea7d0ad84b549a17fe
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-18 16:01:56 +0900

    feat: learn python objects

---
+++ objects.py
@@ -0,0 +1,21 @@
+import array
+import math
+from math import ceil, asin
+print("==== what is object ====")
+# An object has state and method properties
+# Everything is object in python
+
+print(type("hello world"))
+print(type(100))
+print(type(True))
+print(type(array))
+print(type(math))
+
+# Paradigm => Functional Programming and OOP
+# OOP 4concepts => Abstraction, Encapsulation, Inheritance, Polimorphism
+
+result = math.ceil(95.3)  # Call
+print(result)
+
+result1 = ceil(92.3)
+print(result1)

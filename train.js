2cf109ef827e337b063722a19ac422325d8ddafb
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-19 15:54:09 +0900

    fix: python special/magic methods

--- train.js
+++ train.js
@@ -16,19 +16,42 @@
 // const result = countLetter("e", "engineer");
 // console.log(result);
 
-function countLetter(letter, word) {
+// function countLetter(letter, word) {
+//   let counter = 0;
+//   let list = [];
+//   for (let l of word) {
+//     list.push(l);
+//   }
+//   list.map((a) => {
+//     if (letter === a) {
+//       counter++;
+//     }
+//   });
+//   return counter;
+// }
+
+// const result = countLetter("e", "engineer");
+// console.log(result);
+
+// ============================================================================================================
+
+// B-TASK (Nodejs)
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda
+//  qatnashgan raqamlarni sonini bizga return qilsin.
+
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+function countDigits(a) {
   let counter = 0;
-  let list = [];
-  for (let l of word) {
-    list.push(l);
-  }
-  list.map((a) => {
-    if (letter === a) {
+  for (let char of a) {
+    console.log(typeof char);
+    if (char >= "0" && char <= "9") {
       counter++;
     }
-  });
+  }
   return counter;
 }
 
-const result = countLetter("e", "engineer");
+const result = countDigits("ad2a54y79wet0sfgb9");
 console.log(result);

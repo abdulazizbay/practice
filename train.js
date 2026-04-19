a89e27799a5c369de282a552aa728f31bf9e0895
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-20 00:52:05 +0900

    fix: python special/magic methods

--- train.js
+++ train.js
@@ -1,3 +1,27 @@
+// B-TASK (Nodejs)
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda
+//  qatnashgan raqamlarni sonini bizga return qilsin.
+
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+// Yechim
+function countDigits(a) {
+  let counter = 0;
+  for (let char of a) {
+    if (char >= "0" && char <= "9") {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+const result = countDigits("ad2a54y79wet0sfgb9");
+console.log(result);
+
+
+// ===========================================================================================================
+
 /*  A-TASK (NodeJS)
         Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
         MASALAN countLetter("e", "engineer") 3ni return qiladi. 
@@ -32,26 +56,3 @@
 
 // const result = countLetter("e", "engineer");
 // console.log(result);
-
-// ============================================================================================================
-
-// B-TASK (Nodejs)
-
-// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda
-//  qatnashgan raqamlarni sonini bizga return qilsin.
-
-// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
-
-function countDigits(a) {
-  let counter = 0;
-  for (let char of a) {
-    console.log(typeof char);
-    if (char >= "0" && char <= "9") {
-      counter++;
-    }
-  }
-  return counter;
-}
-
-const result = countDigits("ad2a54y79wet0sfgb9");
-console.log(result);

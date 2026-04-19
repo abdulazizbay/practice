93fe70ed34c590859c04c77cc661621be52549be
abdulazizbay7 <abdulazizxalilov30@gmail.com>
2026-04-19 15:40:13 +0900

    fix: python special/magic methods

--- class.about.py
+++ class.about.py
@@ -41,3 +41,45 @@ print("new message:", new_message)
 
 # static method
 Person.explain()
+
+
+print("=== special/magic methods ===")
+
+
+class Car():
+    # state
+    description = "this class makes cars"
+    # constuctor
+
+    def __new__(cls, *args):
+        print("*__*")
+        return super().__new__(cls)
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+    # methods
+
+    def start_engine(self):
+        print(f"the {self.name} started engine")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine")
+
+    def __str__(self):
+        return f"{self.name} was produced in {self.year} year!"
+
+    def __call__(self):
+        print(f"Obj called __call__")
+        return True
+
+
+myCar = Car("Ferrari", 2025)
+myCar.start_engine()
+myCar.stop_engine()
+
+print("-------")
+your_car = Car("Tayota", 2026)
+print(your_car)  # __str__
+response = your_car()  # __call__
+print(response)
